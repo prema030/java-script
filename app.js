eda970062b3823d874ca9c16cc47b36fbfb2ca92
prema <prema@gmail.com>
2025-09-22 10:43:15 +0530

    changes

---
+++ app.js
@@ -0,0 +1,22 @@
+// types of variables
+//var, let,const
+//var-global variable
+//let,const - local varibale
+
+
+
+var a = 67;
+var a = 90;
+let b = 68;
+const c = 78;
+
+console.log('value b is ${b}');
+console.log('value c is ${c}');
+console.log(a);
+//comment -
+
+//commit used to understand the code by the other developer
+//data types
+//1.primitive 2.non-primitive,
+//primitive-Number,String,Boolean,Null,Undefined
+//Non-primitive-Array,Object
